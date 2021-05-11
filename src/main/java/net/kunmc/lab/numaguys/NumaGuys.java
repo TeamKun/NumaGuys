4e8b5adef692a9ce5f30e517ddfc97c3c466906a
BosatsuNumNum <bosatsunumnum@gmail.com>
2021-05-11 22:33:17 +0900

    ゲームタスクのベースを実装

--- src/main/java/net/kunmc/lab/numaguys/NumaGuys.java
+++ src/main/java/net/kunmc/lab/numaguys/NumaGuys.java
@@ -1,5 +1,6 @@
 package net.kunmc.lab.numaguys;
 
+import net.kunmc.lab.numaguys.game.GameTask;
 import net.kunmc.lab.numaguys.util.Const;
 import net.kunmc.lab.numaguys.command.CommandController;
 import net.kunmc.lab.numaguys.command.TabComplete;
@@ -17,6 +18,9 @@ public final class NumaGuys extends JavaPlugin {
         // コマンド読み込み
         getCommand(Const.COMMAND_NUMA_GUYS).setExecutor(new CommandController());
         getCommand(Const.COMMAND_NUMA_GUYS).setTabCompleter(new TabComplete());
+
+        // ゲームタスク起動
+        new GameTask().runTaskTimer(this, 0, 5);
 
     }
 
---
+++ src/main/java/net/kunmc/lab/numaguys/game/GameTask.java
@@ -0,0 +1,15 @@
+package net.kunmc.lab.numaguys.game;
+
+import net.kunmc.lab.numaguys.stage.Stage;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class GameTask extends BukkitRunnable {
+
+    /** ステージ */
+    public static Stage stage;
+
+    @Override
+    public void run() {
+
+    }
+}
