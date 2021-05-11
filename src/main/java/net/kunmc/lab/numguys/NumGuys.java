a2c135a10d203df26af7ce0577c389711d9c32ee
BosatsuNumNum <bosatsunumnum@gmail.com>
2021-05-11 19:15:39 +0900

    初回コミット

---
+++ src/main/java/net/kunmc/lab/numguys/NumGuys.java
@@ -0,0 +1,17 @@
+package net.kunmc.lab.numguys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NumGuys extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
